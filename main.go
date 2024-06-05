85802c222d6ed19313152448b6e80638866127e8
Adrian PK <adrian.git@adrianpk.com>
2024-06-05 17:02:54 +0200

    Implement a single dispatcher

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type List struct {
@@ -14,32 +16,72 @@ type List struct {
 var lists []List
 
 func main() {
-	lists = []List{}
+	lists = []List{
+		List{
+			Name: "Groceries",
+			Items: []string{
+				"Apples",
+				"Oranges",
+				"Bananas",
+			},
+		},
+	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/todos", dispatch)
+
+	mux.HandleFunc("/", showHome)
+	mux.HandleFunc("/todos/", handleTodos)
+	mux.HandleFunc("/todos", handleTodos)
+	// mux.HandleFunc("/todos/", showTodoEdit)
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
-func dispatch(w http.ResponseWriter, r *http.Request) {
+func handleTodos(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimPrefix(r.URL.Path, "/todos")
+	if path != "/" {
+		switch r.Method {
+		case http.MethodGet:
+			showTodo(w, r)
+		case http.MethodPut:
+			updateTodo(w, r)
+		default:
+			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		}
+		return
+	}
+
 	switch r.Method {
 	case http.MethodGet:
-		showLists(w, r)
+		showTodos(w, r)
 	case http.MethodPost:
-		createList(w, r)
+		createTodo(w, r)
 	default:
 		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
 	}
 }
 
-func showLists(w http.ResponseWriter, r *http.Request) {
+func showHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi Everybody!")
+}
+
+func showTodos(w http.ResponseWriter, r *http.Request) {
 	for _, list := range lists {
 		fmt.Fprintf(w, "%s\n", list.Name)
 	}
 }
 
-func createList(w http.ResponseWriter, r *http.Request) {
+func showTodo(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/todos/")
+	id, _ := strconv.Atoi(idStr)
+	if id < len(lists) {
+		fmt.Fprintf(w, "%v", lists[id])
+	} else {
+		http.Error(w, "List not found", http.StatusNotFound)
+	}
+}
+
+func createTodo(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
 
 	list := List{
@@ -49,4 +91,16 @@ func createList(w http.ResponseWriter, r *http.Request) {
 	lists = append(lists, list)
 
 	fmt.Fprintf(w, "List created: %s\n", list.Name)
+}
+
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/todos/")
+	id, _ := strconv.Atoi(idStr)
+	if id < len(lists) {
+		lists[id].Name = r.FormValue("name")
+		http.Redirect(w, r, fmt.Sprintf("/todos/%d", id), http.StatusSeeOther)
+		return
+	}
+
+	http.Error(w, "List not found", http.StatusNotFound)
 }
